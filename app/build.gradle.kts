d3c73284741290f7a276af33a0131d75288f4ab8
Satoshi Iwai <24517539+bvlion@users.noreply.github.com>
2026-09-14 10:03:09 +0900

    set version 1.0.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
     minSdk = 31
     targetSdk = 37
     versionCode = 1
-    versionName = "1.0"
+    versionName = "1.0.0"
   }
 
   val releaseSigningConfig = if (releaseKeystoreFile.exists()) {
